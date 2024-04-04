63eb2adf8c210f396fcb46ca9a23b3e62071f7e0
Şevki <sevkialtindas2666@gmail.com>
2024-04-05 01:13:55 +0200

    modala form eklendi

--- src/components/AddModal.jsx
+++ src/components/AddModal.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Form } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 
@@ -14,15 +15,28 @@ function AddModal({ showModal, handleClose }) {
         <Modal.Header closeButton>
           <Modal.Title>Modal heading</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Patient Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter Patient Name" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Date</Form.Label>
+              <Form.Control type="datetime-local" placeholder="Password" />
+            </Form.Group>
+
+            <div className="text-center ">
+              <Button variant="success" type="submit" className="me-2">
+                Save
+              </Button>
+              <Button variant="danger" onClick={handleClose}>
+                Close
+              </Button>
+            </div>
+          </Form>
+        </Modal.Body>
       </Modal>
     </>
   );
